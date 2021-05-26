008b6dad4455f9ca543e94f274697d627c1df2cf
Daniel Rivas <daniel.rivas@hey.com>
2021-05-26 18:51:52 +0100

    Processing code passes tests

---
+++ src/utils/parseLog.test.ts
@@ -0,0 +1,32 @@
+import { LogState, parseLog } from "./parseLog";
+import { readFile } from "fs/promises";
+describe("parseLog", () => {
+  it("parses short log correctly", () => {
+    const testString = `/help_page/1 126.318.035.038
+/contact 184.123.665.067
+/home 184.123.665.067
+/about/2 444.701.448.104
+/help_page/1 929.398.951.889
+/index 444.701.448.104
+/help_page/1 929.398.951.889
+/help_page/1 722.247.931.582
+`;
+
+    let state: LogState | undefined = undefined;
+    for (const iterationState of parseLog(testString)) {
+      state = iterationState;
+    }
+
+    expect(state?.get("/help_page/1")?.uniqueViews).toEqual(3);
+    expect(state?.get("/help_page/1")?.views).toEqual(4);
+  });
+
+  it("doesn't choke on long files", async () => {
+    const logs = await readFile("test/webserver.log", "utf-8");
+
+    expect(() => {
+      for (const _ of parseLog(logs, 512)) {
+      }
+    }).not.toThrow();
+  });
+});
--- src/utils/parseLog.ts
+++ src/utils/parseLog.ts
@@ -1,41 +1,60 @@
 const BUF_SIZE = 4096; // 4 kb (assuming ascii input)
 
-type PathState = { views: number; uniqueViews: number; ipSet: Set<string> };
-type LogState = Map<string, PathState>;
+export type PathState = {
+  views: number;
+  uniqueViews: number;
+  ipSet: Set<string>;
+};
+export type LogState = Map<string, PathState>;
 
 /**
  * The log file could conceivably be very long, so we parse it in a streaming fashion.
  * The generator yields periodically to report its current state so the UI can be
  *  updated without having to wait for the entire string to be processed.
  */
-function* parseLog(input: string, bufSize = BUF_SIZE) {
+export function* parseLog(input: string, bufSize = BUF_SIZE) {
   const state: LogState = new Map();
 
+  // if there's no input, do nothing.
   if (input.length === 0) {
     yield state;
     return;
   }
 
+  // if the input is short, handle it in one go.
+  if (bufSize > input.length) {
+    const parts = input.trim().split("\n");
+    processLines(parts, state);
+    yield state;
+    return;
+  }
+
+  // if the input is long, handle it in chunks, yielding incremental updates.
+
   let buf = "";
   let start = 0;
-  let end = Math.min(bufSize, input.length - 1);
+  let end = Math.min(bufSize, input.length);
 
-  while (end <= input.length - 1) {
-    const slice = buf + input.slice(start, end);
+  while (end < input.length - 1) {
+    end = Math.min(start + bufSize, input.length - 1);
+    const slice = buf + input.trimEnd().slice(start, end);
     const parts = slice.split("\n");
     buf = parts.pop() ?? "";
     processLines(parts, state);
 
     start = end;
-    end = Math.min(start + bufSize, input.length - start - 1);
 
     yield state;
   }
+
+  // Last buf must be a line (or malformed text, in which case throw);
+  processLines([buf], state);
+  yield state;
 }
 
 function processLines(lines: string[], state: LogState) {
   for (const line of lines) {
-    const parts = line.split(" ");
+    const parts = line.trim().split(" ");
     if (parts.length !== 2) {
       throw new Error(`Invalid input to processLines: ${line}`);
     }
